8b385748ee406659ff94d1154b494039df956faf
amanda <amanda_reyesb@hotmail.com>
2022-11-23 20:39:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <a class="buttonNavBar" href="index.html">
+            Home
+          </a>
+          <a class="buttonNavBar" href="acercade.html">
+            Acerca de
+          </a>
+          <a class="buttonNavBar" href="contacto.html">
+            Contacto
+          </a>
+        </nav>
+        <h1 align="center">La cocina de Juan</h1>
       </header>
-    </div>
+
+      <h2 class="Tittle2">Acerca de</h2>
+      {/* <center>
+        <img
+          src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elle-decorar-cocinas-pequenas-1568135841.jpg?crop=1.00xw:0.820xh;0,0.107xh&resize=980:*"
+          alt=""
+          width="400px"
+          height="300px"
+        />
+      </center> */}
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium corporis eligendi laboriosam et,
+        vero veniam excepturi necessitatibus, maiores pariatur porro sapiente tempora sunt. Nostrum voluptatem
+        sit vitae quia reprehenderit mollitia.
+      </p>
+
+      <p >dsdsdsd</p>
+
+      <footer>
+        <p align="center">Derechos reservados @2022</p>
+      </footer>
+    </>
   );
 }
 
